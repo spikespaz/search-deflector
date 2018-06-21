c93b18e3fd2ba4ad94dfe5b8ab326f4eda03ee74
spikespaz <spikespaz@outlook.com>
2018-06-21 12:56:37 -0700

    Add input validation to selection function

--- deflector.d
+++ deflector.d
@@ -2,11 +2,23 @@ import std.stdio : write, writeln, readln;
 import std.windows.registry;
 import std.algorithm.sorting : sort;
 import std.range : array, enumerate;
-import std.string : leftJustify;
-import std.conv : to, parse;
+import std.conv : to, parse, ConvException;
+import std.string : toLower, strip;
 
-void main(string[] args) {
+void main() {
     writeln(getBrowserChoice(getAvailableBrowsers()));
+}
+
+int getValidatedInput(string input, int maxValue) {
+    try {
+        const int result = parse!(int)(input);
+        if (result > maxValue || result < 1)
+            return -1;
+        else
+            return result;
+    }
+    catch (ConvException)
+        return -1;
 }
 
 string getBrowserChoice(string[string] browsers) {
@@ -20,11 +32,31 @@ string getBrowserChoice(string[string] browsers) {
     writeln('[' ~ to!(string)(choices.length + 1) ~ "]: Microsoft Edge");
     writeln('[' ~ to!(string)(choices.length + 2) ~ "]: System Default");
 
-
     write("\nSelection: ");
-    string selection = readln();
+    int selection = getValidatedInput(readln(), choices.length + 2);
 
-    return choices[parse!(int)(selection) - 1];
+    while (selection == -1) {
+        write("Please make a valid selection: ");
+        selection = getValidatedInput(readln(), choices.length + 2);
+    }
+
+    string choice;
+
+    if (selection == choices.length + 1)
+        choice = "Microsoft Edge";
+    else if (selection == choices.length + 2)
+        choice = "System Default";
+    else
+        choice = choices[selection - 1];
+
+    write("\nYou chose '" ~ choice ~ "', is this correct? (Y/n): ");
+
+    if (readln().strip().toLower() == "n") {
+        writeln();
+        return getBrowserChoice(browsers);
+    }
+    else
+        return choice;
 }
 
 string[string] getAvailableBrowsers() {
