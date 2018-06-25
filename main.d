124f3483195a9014ed7e8621c92b62c44d4d42cb
spikespaz <spikespaz@outlook.com>
2018-06-24 20:49:44 -0700

    Add try/catch to warn user of crash message

--- main.d
+++ main.d
@@ -1,7 +1,6 @@
 module main;
 
 import std.stdio : writeln;
-import std.windows.registry;
 
 void main(string[] args) {
     if (args.length > 1) {
@@ -10,8 +9,20 @@ void main(string[] args) {
         deflect(args[1]);
     }
     else {
-        import setup : setup;
+        try {
+            import setup : setup;
 
-        setup(args[0]);
+            setup(args[0]);
+        }
+        catch (Exception error) {
+            writeln("\nThe SearchDeflector setup has crashed. Try running the executable as administrator.\n",
+                    "If the problem persists, please copy the error below and submit an issue on GitHub.\n",
+                    "https://github.com/spikespaz/search-deflector/issues\n\n",
+                    "=== BEGIN CRASH EXCEPTION ===\n\n", error, "\n\n=== END CRASH EXCEPTION ===");
+        }
+        finally {
+            writeln("\nSearch Deflector setup completed. You may now close this terminal.");
+            while (true) { }
+        }
     }
 }
