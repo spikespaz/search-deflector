c8401c51023338a8d7afc4f6e9043fba8c8fc893
spikespaz <spikespaz@outlook.com>
2018-06-27 11:52:43 -0700

    Add executable icon

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 dmd main.d deflect.d setup.d -O -release -inline -boundscheck=off
 [ -e main.obj ] && rm main.obj
+rcedit main.exe --set-icon icons/icon.ico
 mv main.exe SearchDeflector.exe
