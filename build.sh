4bafda1c022b21443254e0f16f59df7f6fd2b4b3
spikespaz <spikespaz@outlook.com>
2018-06-27 16:45:17 -0700

    Add check for main.exe in build script

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 dmd main.d deflect.d setup.d -O -release -inline -boundscheck=off
 [ -e main.obj ] && rm main.obj
-rcedit main.exe --set-icon icons/icon.ico
-mv main.exe SearchDeflector.exe
+[ -e main.exe ] &&
+    rcedit main.exe --set-icon icons/icon.ico &&
+    mv main.exe SearchDeflector.exe
