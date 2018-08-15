209ca30cb7e14532704e9b4f2a097e9cffb21933
spikespaz <spikespaz@outlook.com>
2018-08-15 15:03:58 -0700

    Add semver comparison func

---
+++ source/updater.d
@@ -0,0 +1,26 @@
+import std.string: split;
+import std.range: zip, popFront;
+import std.conv: to;
+
+/// Compare two semantic versions, returning true if the first version is newer, false otherwise.
+public bool compareVersions(const string firstVer, const string secondVer) {
+    ushort[] firstVerParts = firstVer.split('.').to!(ushort[]);
+    ushort[] secondVerParts = secondVer.split('.').to!(ushort[]);
+
+    while (firstVerParts.length > secondVerParts.length) {
+        if (firstVerParts[0] != 0) return true;
+        firstVerParts.popFront();
+    }
+
+    while (secondVerParts.length > firstVerParts.length) {
+        if (secondVerParts[0] != 0) return false;
+        secondVerParts.popFront();
+    }
+
+    foreach (verParts; zip(firstVerParts, secondVerParts)) {
+        if (verParts[0] > verParts[1]) return true;
+        else if (verParts[1] > verParts[0]) return false;
+    }
+
+    return false;
+}
