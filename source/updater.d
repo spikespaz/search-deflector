c535361ba810a5747b035296505a055111071c26
spikespaz <spikespaz@outlook.com>
2018-08-15 18:22:59 -0700

    Get version data from gh repo

--- source/updater.d
+++ source/updater.d
@@ -1,6 +1,39 @@
+module updater;
+
+import std.json;
+import std.net.curl;
 import std.string: split;
+import std.format: format;
 import std.range: zip, popFront;
+import std.algorithm: sort;
 import std.conv: to;
+
+version (Win64)
+    private enum bool COMPILED_64 = true;
+else
+    private enum bool COMPILED_64;
+
+private enum string RELEASES_URL = "https://api.github.com/repos/%s/%s/releases";
+
+/// Get all current releases as JSON from the GitHub API.
+public JSONValue getReleases(const string author, const string repository) {
+    return get(RELEASES_URL.format(author, repository)).parseJSON();
+}
+
+/// Return a JSONValue array sorted by the `tag_name` as semantic versions.
+public JSONValue[] getSortedReleases(const string author, const string repository) {
+    JSONValue jsonData = getReleases(author, repository);
+    JSONValue[] releasesArray = jsonData.array;
+
+    releasesArray.sort!((firstVer, secondVer) => compareVersions(firstVer["tag_name"].str, secondVer["tag_name"].str));
+
+    return releasesArray;
+}
+
+/// Return the latest release according to semantic versioning.
+public JSONValue getLatestRelease(const string author, const string repository) {
+    return getSortedReleases(author, repository)[0];
+}
 
 /// Compare two semantic versions, returning true if the first version is newer, false otherwise.
 public bool compareVersions(const string firstVer, const string secondVer) {
@@ -23,4 +56,11 @@ public bool compareVersions(const string firstVer, const string secondVer) {
     }
 
     return false;
+}
+
+void main() {
+    import std.stdio;
+
+    foreach (release; getSortedReleases("spikespaz", "example-releases"))
+        writeln(release["tag_name"].str);
 }
