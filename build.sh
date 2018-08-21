0d018a497ad3cc58b6d6126dfd82d3a86ee019e9
spikespaz <spikespaz@outlook.com>
2018-08-20 22:20:33 -0700

    Use branch name in build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
-release="$(git describe --tags --abbrev=0)-$(git rev-parse --short HEAD)"
+
+release="$(git describe --tags --abbrev=0)-$(git rev-parse --abbrev-ref HEAD)"
 mkdir -p "build/$release"
 
 # ldc2 "source/setup.d" -of="build/$release/setup.exe" \
 #     -L/SUBSYSTEM:WINDOWS -O3 -ffast-math -release
-ldc2 "source/launcher.d" -of="build/$release/launcher.exe" \
+ldc2 "source/launcher.d" "source/common.d" -of="build/$release/launcher.exe" \
     -L/SUBSYSTEM:WINDOWS -O3 -ffast-math -release
 # ldc2 "source/deflector.d" -of="build/$release/deflector.exe" \
 #     -L/SUBSYSTEM:WINDOWS -O3 -ffast-math -release
@@ -13,4 +14,6 @@ find "build/$release" -name "*.obj" -delete
 
 [ -e "build/$release/launcher.exe" ] && \
     rcedit "build/$release/launcher.exe" --set-icon "icons/icon.ico"
+
+cp "build/$release/launcher.exe" "build/"
 cp $(which libcurl.dll) "build/$release/libcurl.dll"
