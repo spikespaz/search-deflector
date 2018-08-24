d1bbecd1a77369294614a2f7f95bc06362cb1c52
spikespaz <spikespaz@outlook.com>
2018-08-24 06:45:55 -0700

    Add setup.d and engines.txt to build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 release="0.0.0-$(git rev-parse --abbrev-ref HEAD)"
 mkdir -p "build/$release"
 
-# ldc2 "source/setup.d" -of="build/$release/setup.exe" \
-#     -L/SUBSYSTEM:WINDOWS -O3 -ffast-math -release
+ldc2 "source/setup.d" "source/common.d" -of="build/$release/setup.exe" \
+    -O3 -ffast-math -release
 ldc2 "source/launcher.d" "source/common.d" -of="build/$release/launcher.exe" \
     -L/SUBSYSTEM:WINDOWS -O3 -ffast-math -release
 ldc2 "source/updater.d" "source/common.d" -of="build/$release/updater.exe" \
@@ -15,8 +15,10 @@ ldc2 "source/deflector.d" "source/common.d" -of="build/$release/deflector.exe" \
 
 find "build/$release" -name "*.obj" -delete
 
+[ -e "build/$release/setup.exe" ] && \
+    rcedit "build/$release/setup.exe" --set-icon "icons/icon.ico"
 [ -e "build/$release/launcher.exe" ] && \
     rcedit "build/$release/launcher.exe" --set-icon "icons/icon.ico"
 
-cp "build/$release/launcher.exe" "build/"
-cp $(which libcurl.dll) "build/$release/libcurl.dll"
+cp "engines.txt" "build/$release"
+cp $(which libcurl.dll) "build/$release"
