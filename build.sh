3e78da78da97b3d317a4b49810c5cee6e6140d09
spikespaz <spikespaz@outlook.com>
2018-08-26 19:24:22 -0700

    Fix build script when zero args passed

--- build.sh
+++ build.sh
@@ -5,11 +5,7 @@ release="$(git describe --tags --abbrev=0)-$(git rev-parse --abbrev-ref HEAD)"
 echo "Creating the build path: build/$release"
 mkdir -p "build/$release"
 
-if [[ "$#" -eq 0 ]]; then
-    compile=("setup", "launcher", "updater", "deflector")
-fi
-
-if [[ "$@" = "setup" ]]; then
+if [ "$#" -eq 0 ] || [ "$@" = "setup" ]; then
     echo "Compiling executable: build/$release/setup.exe"
     ldc2 "source/setup.d" "source/common.d" -of="build/$release/setup.exe" \
         -O3 -ffast-math -release -g
@@ -19,7 +15,7 @@ if [[ "$@" = "setup" ]]; then
         rcedit "build/$release/setup.exe" --set-icon "icons/icon.ico"
 fi
 
-if [[ "$@" = "launcher" ]]; then
+if [ "$#" -eq 0 ] || [ "$@" = "launcher" ]; then
     echo "Compiling executable: build/$release/launcher.exe"
     ldc2 "source/launcher.d" "source/common.d" -of="build/$release/launcher.exe" \
         -O3 -ffast-math -release -g
@@ -29,13 +25,13 @@ if [[ "$@" = "launcher" ]]; then
         rcedit "build/$release/launcher.exe" --set-icon "icons/icon.ico"
 fi
 
-if [[ "$@" = "updater" ]]; then
+if [ "$#" -eq 0 ] || [ "$@" = "updater" ]; then
     echo "Compiling executable: build/$release/updater.exe"
     ldc2 "source/updater.d" "source/common.d" -of="build/$release/updater.exe" \
         -O3 -ffast-math -release -g
 fi
 
-if [[ "$@" = "deflector" ]]; then
+if [ "$#" -eq 0 ] || [ "$@" = "deflector" ]; then
     echo "Compiling executable: build/$release/deflector.exe"
     ldc2 "source/deflector.d" "source/common.d" -of="build/$release/deflector.exe" \
         -O3 -ffast-math -release -g
