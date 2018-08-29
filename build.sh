7e1b2f8d7af423eb89486d7d31f26a568b0cc80f
spikespaz <spikespaz@outlook.com>
2018-08-29 16:45:21 -0700

    Allow multiple modules to be built at once

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
-#! /bin/sh
+#! /bin/sh -e
 
 # release="0.0.0-$(git rev-parse --abbrev-ref HEAD)"
 release="$(git describe --tags --abbrev=0)-$(git rev-parse --abbrev-ref HEAD)"
+
 echo "Creating the build path: build/$release"
+
 mkdir -p "build/$release"
 
-if [ "$#" -eq 0 ] || [ "$@" = "setup" ]; then
+function setup {
     echo "Compiling executable: build/$release/setup.exe"
     ldc2 "source/setup.d" "source/common.d" -of="build/$release/setup.exe" \
         -O3 -ffast-math -release -g
@@ -13,9 +15,9 @@ if [ "$#" -eq 0 ] || [ "$@" = "setup" ]; then
     echo "Adding icon to executable: build/$release/setup.exe"
     [ -e "build/$release/setup.exe" ] && \
         rcedit "build/$release/setup.exe" --set-icon "icons/icon.ico"
-fi
+}
 
-if [ "$#" -eq 0 ] || [ "$@" = "launcher" ]; then
+function launcher {
     echo "Compiling executable: build/$release/launcher.exe"
     ldc2 "source/launcher.d" "source/common.d" -of="build/$release/launcher.exe" \
         -O3 -ffast-math -release -g
@@ -23,18 +25,56 @@ if [ "$#" -eq 0 ] || [ "$@" = "launcher" ]; then
     echo "Adding icon to executable: build/$release/launcher.exe"
     [ -e "build/$release/launcher.exe" ] && \
         rcedit "build/$release/launcher.exe" --set-icon "icons/icon.ico"
-fi
+}
 
-if [ "$#" -eq 0 ] || [ "$@" = "updater" ]; then
+function updater {
     echo "Compiling executable: build/$release/updater.exe"
     ldc2 "source/updater.d" "source/common.d" -of="build/$release/updater.exe" \
         -O3 -ffast-math -release -g
-fi
+}
 
-if [ "$#" -eq 0 ] || [ "$@" = "deflector" ]; then
+function deflector {
     echo "Compiling executable: build/$release/deflector.exe"
     ldc2 "source/deflector.d" "source/common.d" -of="build/$release/deflector.exe" \
         -O3 -ffast-math -release -g
+}
+
+function clean {
+    echo "Removing debug files."
+    find "build/$release" -name "*.pdb" -delete
+}
+
+if [ "$#" -eq 0 ]; then
+    setup
+    launcher
+    updater
+    deflector
+else
+    while [ "$#" -gt 0 ]; do
+    param="$1"
+
+    case $param in
+        s|setup)
+            setup
+            shift
+        ;;
+        l|launcher)
+            launcher
+            shift
+        ;;
+        u|updater)
+            updater
+            shift
+        ;;
+        d|deflector)
+            deflector
+            shift
+        ;;
+        c|clean)
+            clean
+            shift
+    esac
+    done
 fi
 
 echo "Removing residual object files."
