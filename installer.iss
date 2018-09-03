f95872cc6ef7e1c97ce9bf2c45e6629fbfa9deaf
spikespaz <spikespaz@outlook.com>
2018-09-02 19:29:48 -0700

    Increment patch number

--- installer.iss
+++ installer.iss
@@ -1,4 +1,4 @@
-#define AppVersion "0.2.0"
+#define AppVersion "0.2.1"
 #define AppBranch "master"
 
 [Setup]
--- source/common.d
+++ source/common.d
@@ -15,7 +15,7 @@ import std.utf: toUTF16z;
 import std.conv: to;
 
 /// Public version strings determined at compile time.
-enum string VERSION = "0.2.0-master";
+enum string VERSION = "0.2.1-master";
 enum string UPDATE_FILE = "SearchDeflector-Installer.exe";
 enum string RELEASES_URL = "https://api.github.com/repos/%s/%s/releases";
 
