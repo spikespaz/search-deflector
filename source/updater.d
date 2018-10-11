090067615bdc2c9fc447e9a4545259c4db26adee
Jacob Birkett <spikespaz@outlook.com>
2018-10-10 18:44:06 -0700

    Remove commented code and add todo comments

--- source/updater.d
+++ source/updater.d
@@ -7,6 +7,11 @@ import std.file: tempDir, thisExePath;
 import std.net.curl: get, download;
 import std.stdio: writeln;
 
+/* TODO:
+    These variables should be somewhat dynamic, and used as compile-time
+    constants in variables.d somewhat like common.d currently is.
+*/
+
 /// File name of the executable to download and run to install an update.
 enum string SETUP_FILENAME = "SearchDeflector-Installer.exe";
 /// Repository path information for Search Deflector, https://github.com/spikespaz/search-deflector.
@@ -103,62 +108,20 @@ public bool compareVersions(const string firstVer, const string secondVer) {
     return false;
 }
 
-// import std.windows.registry: Key, Registry, REGSAM, RegistryException;
-// import core.sys.windows.windows: ShellExecuteA, SW_SHOWNORMAL;
-// import std.path: buildPath, dirName;
-// import common: createErrorDialog;
-// import std.process: wait, spawnProcess;
-// import std.net.curl: download;
-// import std.string: toStringz;
-// import std.file: thisExePath;
-// import std.file: tempDir;
+/* TODO:
+    Abstract the shell command replacement into the setup executable,
+    since this file changed to only execute the installer.
 
-// void main(string[] args) {
-//     try {
-//         if (args.length == 1) {
-//             if (checkConfigured())
-//                 setShellCommand(buildPath(thisExePath().dirName(), "launcher.exe"));
-//             else // Assumes already runnig as admin.
-//                 spawnProcess(buildPath(thisExePath().dirName(), "setup.exe")).wait();
-//         } else
-//             update(args[1]);
-//     } catch (Exception error)
-//         createErrorDialog(error);
-// }
+    That post-install functionality needs to be moved into the setup
+    so that when the installer calls it, it can fix the registry settings
+    to point to the new location or version folder autonomously.
 
-// /// New update function to download and run the installer instead of unpacking the zip.
-// void update(const string downloadUrl) {
-//     const string downloadPath = buildPath(tempDir, "SearchDeflector-Installer.exe");
+    This should work by checking if the config key in the registry exists.
+    If it does, make sure the "shell open" command points to the latest deflector.
 
-//     download(downloadUrl, downloadPath);
+    Also, once the setup is rewritten to use the Task Scheduler to run the updater,
+    update that task to point to the new updater.
 
-//     ShellExecuteA(null, "runas".toStringz(), downloadPath.toStringz(), null, null, SW_SHOWNORMAL);
-// }
-
-// /// Set the new file association.
-// public void setShellCommand(const string filePath) {
-//     Key shellCommandKey;
-
-//     try
-//         shellCommandKey = Registry.classesRoot.getKey("SearchDeflector\\shell\\open\\command", REGSAM.KEY_WRITE);
-//     catch (RegistryException)
-//         shellCommandKey = Registry.classesRoot.createKey("SearchDeflector\\shell\\open\\command", REGSAM.KEY_WRITE);
-
-//     shellCommandKey.setValue("", '"' ~ filePath ~ "\" \"%1\"");
-
-//     shellCommandKey.flush();
-// }
-
-// /// Checks if the required registry values already exist.
-// bool checkConfigured() {
-//     try {
-//         Key deflectorKey = Registry.currentUser.getKey("SOFTWARE\\Clients\\SearchDeflector");
-
-//         deflectorKey.getValue("BrowserPath");
-//         deflectorKey.getValue("EngineURL");
-//         deflectorKey.getValue("LastUpdateCheck");
-
-//         return true;
-//     } catch (RegistryException)
-//         return false;
-// }
+    Should be ported from the old code. For reference, I'll link it.
+    https://github.com/spikespaz/search-deflector/blob/0.2.3/source/updater.d
+*/
