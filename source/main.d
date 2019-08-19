7935a70a2f177a08f573a71f1eac20897da7169f
Jacob Birkett <spikespaz@outlook.com>
2019-08-19 00:49:11 -0700

    Add getters and setters for margins in layout

--- source/main.d
+++ source/main.d
@@ -1,11 +1,14 @@
 import arsd.minigui;
+import std.windows.registry: RegistryException;
+import std.stdio: writeln;
 import common: getConsoleArgs, createErrorDialog;
-import setup: getAvailableBrowsers;
+import setup: getAvailableBrowsers, getAvailableBrowsers;
+
 
 void main(string[] args) {
     try {
-        auto window = new MainWindow("Search Deflector", 500, 500);
-        auto layout = new PaddedVerticalLayout(window);
+        auto window = new MainWindow("Search Deflector", 400, 250);
+        auto layout = new NewVerticalLayout(window);
 
         auto textLabel0 = new TextLabel("Preferred Browser", TextAlignment.Left, layout);
         auto browserSelect = new DropDownSelection(layout);
@@ -25,31 +28,42 @@ void main(string[] args) {
 
         auto applyButton = new Button("Apply Settings", layout);
 
+        layout.setMargins(4, 8, 4, 8);
+
         window.loop();
     } catch (Exception error) {
         createErrorDialog(error);
     }
 }
 
-class PaddedVerticalLayout : VerticalLayout {
+class NewVerticalLayout : VerticalLayout {
+    private int _marginTop = 0, _marginLeft = 0, _marginBottom = 0, _marginRight = 0;
+
     this(Widget parent = null) {
         super(parent);
     }
 
-    override int marginLeft() {
-        return 10;
+    override int marginTop() {
+        return this._marginTop;
     }
 
-    override int marginTop() {
-        return 10;
+    override int marginLeft() {
+        return this._marginLeft;
     }
 
     override int marginRight() {
-        return 10;
+        return this._marginRight;
     }
 
     override int marginBottom() {
-        return 10;
+        return this._marginBottom;
+    }
+
+    void setMargins(int top, int left, int bottom, int right) {
+        this._marginTop = top;
+        this._marginLeft = left;
+        this._marginBottom = bottom;
+        this._marginRight = right;
     }
 }
 
