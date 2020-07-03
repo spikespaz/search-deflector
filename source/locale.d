08444202b11825317d88fab43134c90d4ee73631
BuildTools <spikespaz@outlook.com>
2020-07-03 14:31:09 -0700

    Add indexing to Translation and function to get lang name

--- source/locale.d
+++ source/locale.d
@@ -1,72 +1,111 @@
 module locale;
 
-import core.sys.windows.winnt: LCID, LANGID, LPWSTR, DWORD, MAKELCID, SORT_DEFAULT;
+import core.sys.windows.winnt: LCID, LANGID, LPWSTR, LPCWSTR, DWORD, MAKELCID, SORT_DEFAULT;
+import core.sys.windows.winnls: GetLocaleInfoW;
 import std.file: thisExePath, readText, dirEntries, SpanMode;
 import std.path: buildPath, dirName, baseName;
 import std.string: fromStringz, toLower;
+import std.utf: toUTF16z;
 import std.conv: to;
 import common: parseConfig;
 debug import std.stdio: writeln;
+debug import std.string: format;
 
 
 extern (Windows) {
     /// https://docs.microsoft.com/en-us/windows/win32/api/winnls/nf-winnls-lcidtolocalename
     int LCIDToLocaleName(LCID, LPWSTR, int, DWORD);
+    /// https://docs.microsoft.com/en-us/windows/win32/api/winnls/nf-winnls-localenametolcid
+    LCID LocaleNameToLCID(LPCWSTR lpName, DWORD dwFlags);
     /// https://docs.microsoft.com/en-us/windows/win32/api/winnls/nf-winnls-getuserdefaultuilanguage
     LANGID GetUserDefaultUILanguage();
 
+    /// https://docs.microsoft.com/en-us/windows/win32/intl/locale-names
     /// https://www.magnumdb.com/search?q=LOCALE_NAME_MAX_LENGTH
     enum uint LOCALE_NAME_MAX_LENGTH = 0x55;
     /// https://docs.microsoft.com/en-us/windows/win32/intl/locale-allow-neutral-names
+    /// https://www.magnumdb.com/search?q=LOCALE_ALLOW_NEUTRAL_NAMES
     enum uint LOCALE_ALLOW_NEUTRAL_NAMES = 0x08000000;
+    /// https://docs.microsoft.com/en-us/windows/win32/intl/locale-slocalized-constants
+    /// https://www.magnumdb.com/search?q=LOCALE_SLOCALIZEDDISPLAYNAME
+    enum uint LOCALE_SLOCALIZEDDISPLAYNAME = 0x00000002;
 }
 
 /// Static object for translation file loading
 static struct Translator {
-    private static string[string] map;
-    private static int lcid;
-    private static string lang_key;
+    private static string[string] translationMap;
+    private static string[string] langFileMap;
+    private static string langKey;
+
+    static this() {
+        foreach (string fileName; dirEntries(buildPath(thisExePath().dirName(), "lang"), SpanMode.shallow)) {
+            debug writeln("Found locale file: " ~ fileName);
+            langFileMap[fileName.baseName().toLower()[0 .. $ - 4]] = fileName;
+        }
+    }
 
     static string getUserDefaultLangKey() {
         wchar[LOCALE_NAME_MAX_LENGTH] localeName;
-
-        LCIDToLocaleName(
-            MAKELCID(GetUserDefaultUILanguage(), SORT_DEFAULT),
-            localeName.ptr,
-            LOCALE_NAME_MAX_LENGTH,
-            LOCALE_ALLOW_NEUTRAL_NAMES
-        );
-
+        LCIDToLocaleName(getDefaultLCID(), localeName.ptr, LOCALE_NAME_MAX_LENGTH, LOCALE_ALLOW_NEUTRAL_NAMES);
         return localeName.ptr.fromStringz().to!string();
     }
 
-    private static string getTranslationFilePath(const string lang) {
-        foreach (string name; dirEntries(buildPath(thisExePath().dirName(), "lang"), SpanMode.shallow)) {
-            debug writeln("Found locale: " ~ name);
-
-            if (name.baseName().toLower() == lang.toLower() ~ ".txt")
-                return name;
-        }
-
-        return null;
+    static uint getDefaultLCID() {
+        return MAKELCID(GetUserDefaultUILanguage(), SORT_DEFAULT);
     }
 
-    /// Finds the user's current preferred language and loads the corresponding file
-    static void load() {
-        lang_key = getUserDefaultLangKey();
-        string file_path = getTranslationFilePath(lang_key);
+    static string getNameFromLangKey(const string langKey) {
+        const uint maxNameLen = 255;
+        wchar[maxNameLen] langName;
 
-        if (file_path is null) {
+        GetLocaleInfoW(
+            LocaleNameToLCID(langKey.toUTF16z(), LOCALE_ALLOW_NEUTRAL_NAMES),
+            LOCALE_SLOCALIZEDDISPLAYNAME,
+            langName.ptr,
+            maxNameLen
+        );
+
+        debug writeln("langName: " ~ langName);
+        return langName.ptr.fromStringz().to!string();
+    }
+
+    static string[] getLangKeys() {
+        return langFileMap.keys;
+    }
+
+    /// Load translations from file by specified langKey
+    static bool load(const string langKey) {
+        Translator.langKey = langKey;
+        return load();
+    }
+
+    /// Load the file corresponding to the current langKey
+    static bool load() {
+        bool success = true;
+        string filePath = langFileMap.get(langKey, null);
+
+        if (filePath is null) {
             debug writeln("Requested user default locale not found!");
-            file_path = getTranslationFilePath("en-US");
+            filePath = langFileMap["en-us"];
+            success = false;
         }
 
-        map = parseConfig(readText(file_path));
+        translationMap = parseConfig(readText(filePath));
+        return success;
+    }
+
+    /// Load the user's default language, returns success
+    static bool loadDefault() {
+        return load(getUserDefaultLangKey());
     }
 
     /// Return the translation by key
-    static string text(string key) {
+    static string text(const string key) {
         debug writeln("Getting translation for key: " ~ key);
-        return map[key];
+
+        if (key !in translationMap)
+            debug writeln("Key not in translations: " ~ key);
+
+        return translationMap.get(key, "UNKNOWN STRING");
     }
 }
