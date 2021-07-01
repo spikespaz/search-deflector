2185eea5b9b625086ec764bf4abb8f35da21bb8f
Jacob Birkett <spikespaz@outlook.com>
2021-07-01 11:26:01 -0700

    Revert "Add support for launching non-Cortana URLs (fixes #77)"
    This reverts commit 49e833f6eae382acd872889709954d28ca441b90.

--- source/deflector.d
+++ source/deflector.d
@@ -39,9 +39,7 @@ void main(string[] args) {
         );
         string launchUrl;
 
-        if (searchInfo.directUrl !is null)
-            launchUrl = searchInfo.directUrl;
-        else if (searchInfo.enteredUrl !is null)
+        if (searchInfo.enteredUrl !is null)
             launchUrl = searchInfo.enteredUrl;
         else if (searchInfo.selectedUrl !is null)
             launchUrl = searchInfo.selectedUrl;
